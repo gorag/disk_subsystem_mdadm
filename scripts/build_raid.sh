7fcbd752dc609b623b9db250bf710cc92ddf1183
Alexey <gorbunov.alexey@gmail.com>
2019-11-14 01:25:24 +0300

    Script update

--- scripts/build_raid.sh
+++ scripts/build_raid.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
+MNT="/mnt/raid"
+DEV="/dev/md0"
 
-mdadm --create --verbose /dev/md0 -l 5 -n 6 /dev/sd{b,c,d,e,f,g}
+mdadm --create --verbose $DEV -l 5 -n 6 /dev/sd[bcdefg]
 
-parted -s /dev/md0 mklabel gpt
-
+parted -s $DEV mklabel gpt
 for i in $(seq 1 5)
     do
-        parted /dev/md0 mkpart primary ext4 (($i - 1) * 20))
-
-blkid | awk '/dev\/md0/ {print$2, "/mnt/raid10", $3, "defaults", 0, 0}' | sed -r 's/\"|TYPE=//'g >> /etc/fstab
+        parted $DEV mkpart primary ext4 $((($i-1)*20))% $((i*20))%
+        mkfs.ext4 $DEV'p'$i
+        mkdir -p $MNT'/part'$i
+        AWK_CONDITION="/^\/dev\/md0p$i/{print\$5,\"$MNT/part$i\",\$3,\"defaults\",0,0}"
+        blkid | awk $AWK_CONDITION | sed -r 's/\"|TYPE=//'g >> /etc/fstab
+    done
 mount -a
